35d690cf095e168cd9b8fd930b7d376fb906ffab
Ram10 <Rameezmajid10@outlook.com>
2025-07-22 13:29:12 +0100

    Update asyncTimewarp.cs

--- Assets/asyncTimewarp.cs
+++ Assets/asyncTimewarp.cs
@@ -1,8 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.InteropServices;
-using System.Threading;
 using UnityEngine;
 using System;
 
@@ -21,19 +18,20 @@ public class asyncTimewarp : MonoBehaviour
     void Start()
     {
         resolution = new Vector2(Screen.width, Screen.height);
-        targetTexture = makeBackbufferTexture();
-        targetTextureDepth = makeDepthTexture();
+        targetTexture = makeBackbufferTexture(Screen.width, Screen.height);
+        targetTextureDepth = makeDepthTexture(Screen.width, Screen.height);
         cam.SetTargetBuffers(targetTexture.colorBuffer, targetTextureDepth.depthBuffer);
     }
-    RenderTexture makeBackbufferTexture()
+
+    RenderTexture makeBackbufferTexture(int width, int height)
     {
-        return new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.Default);
-    }
-    RenderTexture makeDepthTexture()
-    {
-        return new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
+        return new RenderTexture(width, height, 0, RenderTextureFormat.Default);
     }
 
+    RenderTexture makeDepthTexture(int width, int height)
+    {
+        return new RenderTexture(width, height, 24, RenderTextureFormat.Depth);
+    }
 
     void setCamera()
     {
@@ -43,17 +41,11 @@ public class asyncTimewarp : MonoBehaviour
         DrawTextureMat.SetVector("_CameraForward", cam.transform.forward);
         DrawTextureMat.SetMatrix("_WorldToCameraMatrix", cam.worldToCameraMatrix);
         DrawTextureMat.SetMatrix("_ProjectionMatrix", cam.projectionMatrix);
-        
 
         DrawTextureMat.SetVector("_TopLeft", cam.ViewportPointToRay(new Vector3(0, 1)).direction);
         DrawTextureMat.SetVector("_TopRight", cam.ViewportPointToRay(new Vector3(1, 1)).direction);
         DrawTextureMat.SetVector("_BottomLeft", cam.ViewportPointToRay(new Vector3(0, 0)).direction);
         DrawTextureMat.SetVector("_BottomRight", cam.ViewportPointToRay(new Vector3(1, 0)).direction);
-
-        Debug.DrawRay(cam.transform.position, DrawTextureMat.GetVector("_TopLeft") * 100);
-        Debug.DrawRay(cam.transform.position, DrawTextureMat.GetVector("_TopRight") * 100);
-        Debug.DrawRay(cam.transform.position, DrawTextureMat.GetVector("_BottomLeft") * 100);
-        Debug.DrawRay(cam.transform.position, DrawTextureMat.GetVector("_BottomRight") * 100);
     }
 
     void setFrozenCamera()
@@ -65,12 +57,10 @@ public class asyncTimewarp : MonoBehaviour
         DrawTextureMat.SetMatrix("_FrozenWorldToCameraMatrix", cam.worldToCameraMatrix);
         DrawTextureMat.SetMatrix("_FrozenProjectionMatrix", cam.projectionMatrix);
 
-
         DrawTextureMat.SetVector("_FrozenTopLeft", cam.ViewportPointToRay(new Vector3(0, 1)).direction);
         DrawTextureMat.SetVector("_FrozenTopRight", cam.ViewportPointToRay(new Vector3(1, 1)).direction);
         DrawTextureMat.SetVector("_FrozenBottomLeft", cam.ViewportPointToRay(new Vector3(0, 0)).direction);
         DrawTextureMat.SetVector("_FrozenBottomRight", cam.ViewportPointToRay(new Vector3(1, 0)).direction);
-
     }
 
     bool ReprojectMovement = false;
@@ -79,34 +69,38 @@ public class asyncTimewarp : MonoBehaviour
     float accumulatedDT;
     float finalDT;
     int SlowFPS = 30;
+    float lagIntensity = 0.5f;
+    float renderScale = 1.0f; // New: scale factor for Option 7
+    float t = 0.0f;
+
     void Update()
     {
         DrawTextureMat.SetFloat("_StretchBorders", StretchBorders ? 1.0f : 0.0f);
         DrawTextureMat.SetFloat("_ReprojectMovement", ReprojectMovement ? 1.0f : 0.0f);
-        // resize event
+
         if (resolution.x != Screen.width || resolution.y != Screen.height)
         {
-            targetTexture = makeBackbufferTexture();
-            targetTextureDepth = makeDepthTexture();
+            targetTexture = makeBackbufferTexture(Screen.width, Screen.height);
+            targetTextureDepth = makeDepthTexture(Screen.width, Screen.height);
             cam.SetTargetBuffers(targetTexture.colorBuffer, targetTextureDepth.depthBuffer);
             resolution.x = Screen.width;
             resolution.y = Screen.height;
         }
-        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.LeftWindows) || Input.GetKeyDown(KeyCode.RightWindows))
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-            state = 0;
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-            state = 1;
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            state = 2;
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-            state = 3;
-
+        // Mode switching
+        if (Input.GetKeyDown(KeyCode.Alpha1)) state = 0;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) state = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) state = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha4)) state = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha5)) state = 5;
+        if (Input.GetKeyDown(KeyCode.Alpha6)) state = 6;
+        if (Input.GetKeyDown(KeyCode.Alpha7)) state = 7; // NEW: GPU-bound supersample
 
         cam.depthTextureMode = DepthTextureMode.Depth;
         projectorCamera.transform.rotation = cam.transform.rotation;
@@ -118,140 +112,193 @@ public class asyncTimewarp : MonoBehaviour
         float MouseX = Input.GetAxis("Mouse X");
         float MouseY = -Input.GetAxis("Mouse Y");
 
-        float MovementX = 0;
-        float MovementY = 0;
-        if (Input.GetKey(KeyCode.A))
-            MovementX--;
-        if (Input.GetKey(KeyCode.D))
-            MovementX++;
-        if (Input.GetKey(KeyCode.W))
-            MovementY++;
-        if (Input.GetKey(KeyCode.S))
-            MovementY--;
+        float MovementX = 0, MovementY = 0;
+        if (Input.GetKey(KeyCode.A)) MovementX--;
+        if (Input.GetKey(KeyCode.D)) MovementX++;
+        if (Input.GetKey(KeyCode.W)) MovementY++;
+        if (Input.GetKey(KeyCode.S)) MovementY--;
 
         Application.targetFrameRate = -1;
         QualitySettings.vSyncCount = 0;
 
-
         accumulatedDT += Time.deltaTime;
         accumulatedX += MouseX;
         accumulatedY += MouseY;
-        if (state == 0)
-        {
-            t = 0;
-            setCamera();
-            setFrozenCamera();
-            if (!Cursor.visible)
-            {
-                this.transform.parent.transform.Rotate(Vector3.up, MouseX * sensitivity);
-                this.transform.Rotate(Vector3.right, MouseY * sensitivity, Space.Self);
-                this.transform.parent.Translate(this.transform.parent.forward * MovementY * Time.deltaTime * 5.0f, Space.World);
-                this.transform.parent.Translate(this.transform.parent.right * MovementX * Time.deltaTime * 5.0f, Space.World);
-            }
-            this.GetComponent<Camera>().Render();
-            accumulatedDT = 0;
-            accumulatedX = 0;
-            accumulatedY = 0;
-            finalDT = Time.deltaTime;
-        }
-        else if (state == 1)
-        {
-            t = 0;
-            setCamera();
 
-            if (!Cursor.visible)
-            {
-                this.transform.parent.transform.Rotate(Vector3.up, MouseX * sensitivity);
-                this.transform.Rotate(Vector3.right, MouseY * sensitivity, Space.Self);
-                this.transform.parent.Translate(this.transform.parent.forward * MovementY * Time.deltaTime * 5.0f, Space.World);
-                this.transform.parent.Translate(this.transform.parent.right * MovementX * Time.deltaTime * 5.0f, Space.World);
-            }
-            accumulatedDT = 0;
-            accumulatedX = 0;
-            accumulatedY = 0;
-            finalDT = 1;// Time.deltaTime;
-        }
-        else if (state == 2)
+        // Handle modes
+        if (state == 0) normalRender(MouseX, MouseY, MovementX, MovementY);
+        else if (state == 1) freezeAndWarp(MouseX, MouseY, MovementX, MovementY);
+        else if (state == 2) slowRenderFixed(MouseX, MouseY, MovementX, MovementY);
+        else if (state == 3) slowRenderWarp(MouseX, MouseY, MovementX, MovementY);
+        else if (state == 5) lagSimNoWarp(MouseX, MouseY, MovementX, MovementY);
+        else if (state == 6) lagSimWithWarp(MouseX, MouseY, MovementX, MovementY);
+        else if (state == 7) renderSupersample(MouseX, MouseY, MovementX, MovementY);
+
+        if (Input.GetKeyDown(KeyCode.Q)) drawCustom = !drawCustom;
+    }
+
+    void normalRender(float MouseX, float MouseY, float MX, float MY)
+    {
+        t = 0;
+        setCamera();
+        setFrozenCamera();
+        movePlayer(MouseX, MouseY, MX, MY);
+        this.GetComponent<Camera>().Render();
+        resetAccumulation();
+        finalDT = Time.deltaTime;
+    }
+
+    void freezeAndWarp(float MouseX, float MouseY, float MX, float MY)
+    {
+        t = 0;
+        setCamera();
+        movePlayer(MouseX, MouseY, MX, MY);
+        resetAccumulation();
+        finalDT = 1;
+    }
+
+    void slowRenderFixed(float MouseX, float MouseY, float MX, float MY)
+    {
+        float DeltaTimeTarget = 1.0f / SlowFPS;
+        t += Time.deltaTime;
+        setCamera();
+        setFrozenCamera();
+        if (t > DeltaTimeTarget)
         {
-            float DeltaTime60 = 1.0f / SlowFPS;
-            t += Time.deltaTime;
-            setCamera();
-            setFrozenCamera();
-            if (t > DeltaTime60)
-            {
-                t -= DeltaTime60;
-                if (!Cursor.visible)
-                {
-                    this.transform.parent.transform.Rotate(Vector3.up, accumulatedX * sensitivity);
-                    this.transform.Rotate(Vector3.right, accumulatedY * sensitivity, Space.Self);
-                    this.transform.parent.Translate(this.transform.parent.forward * MovementY * accumulatedDT * 5.0f, Space.World);
-                    this.transform.parent.Translate(this.transform.parent.right * MovementX * accumulatedDT * 5.0f, Space.World);
-                }
-                this.GetComponent<Camera>().Render();
-                finalDT = accumulatedDT;
-                accumulatedDT = 0;
-                accumulatedX = 0;
-                accumulatedY = 0;
-            }
-        }
-        else if (state == 3)
-        {
-            float DeltaTime60 = 1.0f / SlowFPS;
-            t += Time.deltaTime;
-            setCamera();
-            if (!Cursor.visible)
-            {
-                this.transform.parent.transform.Rotate(Vector3.up, MouseX * sensitivity);
-                this.transform.Rotate(Vector3.right, MouseY * sensitivity, Space.Self);
-                this.transform.parent.Translate(this.transform.parent.forward * MovementY * Time.deltaTime * 5.0f, Space.World);
-                this.transform.parent.Translate(this.transform.parent.right * MovementX * Time.deltaTime * 5.0f, Space.World);
-            }
-            if (t > DeltaTime60)
-            {
-                t -= DeltaTime60;
-                if (!Cursor.visible)
-                {
-                    //this.transform.parent.transform.Rotate(Vector3.up, accumulatedX * sensitivity);
-                    //this.transform.Rotate(Vector3.right, accumulatedY * sensitivity, Space.Self);
-                }
-                setFrozenCamera();
-                this.GetComponent<Camera>().Render();
-                finalDT = accumulatedDT;
-                accumulatedDT = 0;
-                accumulatedX = 0;
-                accumulatedY = 0;
-            }
-        }
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            drawCustom = !drawCustom;
+            t -= DeltaTimeTarget;
+            movePlayer(accumulatedX, accumulatedY, MX, MY, accumulatedDT);
+            this.GetComponent<Camera>().Render();
+            finalDT = accumulatedDT;
+            resetAccumulation();
         }
     }
-    
+
+    void slowRenderWarp(float MouseX, float MouseY, float MX, float MY)
+    {
+        float DeltaTimeTarget = 1.0f / SlowFPS;
+        t += Time.deltaTime;
+        setCamera();
+        movePlayer(MouseX, MouseY, MX, MY);
+        if (t > DeltaTimeTarget)
+        {
+            t -= DeltaTimeTarget;
+            setFrozenCamera();
+            this.GetComponent<Camera>().Render();
+            finalDT = accumulatedDT;
+            resetAccumulation();
+        }
+    }
+
+    void lagSimNoWarp(float MouseX, float MouseY, float MX, float MY)
+    {
+        float fluctuation = UnityEngine.Random.Range(1.0f - lagIntensity, 1.0f + lagIntensity);
+        float dynamicFPS = Mathf.Max(5, SlowFPS * fluctuation);
+        float DeltaTimeTarget = 1.0f / dynamicFPS;
+
+        t += Time.deltaTime;
+        setCamera();
+        setFrozenCamera();
+        movePlayer(MouseX, MouseY, MX, MY);
+
+        if (t > DeltaTimeTarget)
+        {
+            t -= DeltaTimeTarget;
+            this.GetComponent<Camera>().Render();
+            finalDT = accumulatedDT;
+            resetAccumulation();
+        }
+    }
+
+    void lagSimWithWarp(float MouseX, float MouseY, float MX, float MY)
+    {
+        float fluctuation = UnityEngine.Random.Range(1.0f - lagIntensity, 1.0f + lagIntensity);
+        float dynamicFPS = Mathf.Max(5, SlowFPS * fluctuation);
+        float DeltaTimeTarget = 1.0f / dynamicFPS;
+
+        t += Time.deltaTime;
+        setCamera();
+        movePlayer(MouseX, MouseY, MX, MY);
+
+        if (t > DeltaTimeTarget)
+        {
+            t -= DeltaTimeTarget;
+            setFrozenCamera();
+            this.GetComponent<Camera>().Render();
+            finalDT = accumulatedDT;
+            resetAccumulation();
+        }
+    }
+
+    // Option 7: GPU-bound supersample rendering
+    void renderSupersample(float MouseX, float MouseY, float MX, float MY)
+    {
+        int width = Mathf.RoundToInt(Screen.width * renderScale);
+        int height = Mathf.RoundToInt(Screen.height * renderScale);
+
+        // Clamp to avoid driver crashes
+        width = Mathf.Clamp(width, 1, 16384);
+        height = Mathf.Clamp(height, 1, 16384);
+
+        if (targetTexture == null || targetTexture.width != width || targetTexture.height != height)
+        {
+            targetTexture = makeBackbufferTexture(width, height);
+            targetTextureDepth = makeDepthTexture(width, height);
+        }
+
+        movePlayer(MouseX, MouseY, MX, MY);
+        setCamera();
+        setFrozenCamera();
+
+        cam.SetTargetBuffers(targetTexture.colorBuffer, targetTextureDepth.depthBuffer);
+        cam.Render();
+
+        Graphics.Blit(targetTexture, (RenderTexture)null);
+        finalDT = Time.deltaTime;
+        resetAccumulation();
+    }
+
+    void movePlayer(float MouseX, float MouseY, float MX, float MY, float scale = -1)
+    {
+        if (scale < 0) scale = Time.deltaTime;
+        if (!Cursor.visible)
+        {
+            this.transform.parent.transform.Rotate(Vector3.up, MouseX * sensitivity);
+            this.transform.Rotate(Vector3.right, MouseY * sensitivity, Space.Self);
+            this.transform.parent.Translate(this.transform.parent.forward * MY * scale * 5.0f, Space.World);
+            this.transform.parent.Translate(this.transform.parent.right * MX * scale * 5.0f, Space.World);
+        }
+    }
+
+    void resetAccumulation()
+    {
+        accumulatedDT = 0;
+        accumulatedX = 0;
+        accumulatedY = 0;
+    }
 
     bool drawCustom = false;
     int state = 0;
-
     float sensitivity = 4.0f;
-    float t = 0.0f;
     bool StretchBorders = false;
-
 
     public Material DrawTextureMat;
     public Texture testTexture;
+
     void OnGUI()
     {
         using (var horizontalScope = new GUILayout.VerticalScope("box"))
         {
-            GUIStyle boldStyle = new GUIStyle(GUI.skin.label);
-            boldStyle.fontStyle = FontStyle.Bold;
+            GUIStyle boldStyle = new GUIStyle(GUI.skin.label) { fontStyle = FontStyle.Bold };
             GUIStyle style = new GUIStyle(GUI.skin.label);
 
             GUILayout.Label("Esc to unlock mouse");
-            GUILayout.Label("1 = Render the game at uncapped fps", (state == 0) ? boldStyle : style);
-            GUILayout.Label("2 = Freeze rendering new frames and distort using async timewarp", (state == 1) ? boldStyle : style);
-            GUILayout.Label("3 = Render the game at " + SlowFPS + " fps", (state == 2) ? boldStyle : style);
-            GUILayout.Label("4 = Render the game at " + SlowFPS + " fps, but apply async timewarp", (state == 3) ? boldStyle : style);
+            GUILayout.Label("1 = Render uncapped fps", (state == 0) ? boldStyle : style);
+            GUILayout.Label("2 = Freeze render & warp", (state == 1) ? boldStyle : style);
+            GUILayout.Label("3 = " + SlowFPS + " fps fixed", (state == 2) ? boldStyle : style);
+            GUILayout.Label("4 = " + SlowFPS + " fps + timewarp", (state == 3) ? boldStyle : style);
+            GUILayout.Label("5 = Lag simulation (no warp)", (state == 5) ? boldStyle : style);
+            GUILayout.Label("6 = Lag simulation (with timewarp)", (state == 6) ? boldStyle : style);
+            GUILayout.Label("7 = GPU-bound supersample mode", (state == 7) ? boldStyle : style);
 
             StretchBorders = GUILayout.Toggle(StretchBorders, "Stretch Timewarp borders");
             ReprojectMovement = GUILayout.Toggle(ReprojectMovement, "Include player movement in reprojection");
@@ -259,10 +306,16 @@ public class asyncTimewarp : MonoBehaviour
             GUILayout.Label("Frame Time: " + Math.Round(finalDT * 1000.0f) + "ms " + Math.Round(1.0f / finalDT) + "fps");
 
             GUILayout.Label("Target FPS: " + SlowFPS);
-            SlowFPS = (int)GUILayout.HorizontalSlider((float)SlowFPS, 2, 200);
+            SlowFPS = (int)GUILayout.HorizontalSlider(SlowFPS, 2, 200);
+
+            GUILayout.Label("Lag Intensity: " + Math.Round(lagIntensity * 100) + "%");
+            lagIntensity = GUILayout.HorizontalSlider(lagIntensity, 0.0f, 1.0f);
+
+            GUILayout.Label("Render Scale (1x–25 x): " + renderScale.ToString("F1") + "x");
+            renderScale = GUILayout.HorizontalSlider(renderScale, 1.0f, 25.0f); // This is the range of render scale
+
             GUILayout.Label("Mouse Sensitivity: " + sensitivity);
             sensitivity = GUILayout.HorizontalSlider(sensitivity, 0, 10.0f);
-
 
             if (Cursor.visible && GUILayout.Button("Click to control player"))
             {
